d167d3907d0ead76179b9a07d60f4b96d0bdea0e
Ilya <rabota003@yandex.ru>
2022-12-12 18:55:20 +0300

    two commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        //task1
+        for (int i=0; i<=10; i++) {
+            System.out.println("Иттерация цикла " + i);
+        }
+
+        //task2
+        for (int a =10; a>=0; a--) {
+            System.out.println("Иттерация цикла "+ a);
+        }
+        //task3
+        for (int f = 0; f<=17; f++) {
+            if (f%2=0) {
+                System.out.println(" Четные число " + f);
+        }
+    }
+}
